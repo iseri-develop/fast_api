23c290ed638633ee85918d38b96ab99cec029af0
Igor <iseri.developer@gmail.com>
2024-10-04 19:03:12 -0300

    estudo de endpoints

--- server.py
+++ server.py
@@ -3,9 +3,20 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # decorator
-@app.get("/")
-
-async def root():
+@app.get('/')
+def root():
     return {
         "mensagem": "Olá FastAPI"
+    }
+
+@app.get('/profile')
+def profile():
+    return {
+        "nome": "Igor Iseri"
+    }
+    
+@app.post('/profile')
+def signup():
+    return {
+        "mensagem": "Perfil enviado com sucesso"
     }
